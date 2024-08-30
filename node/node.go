138bd3aea5fe390f53fff20ee1406ddf59603c42
pengzy1008 <pengzy1008@icloud.com>
2024-08-30 11:42:20 +0800

    修改虚拟节点处理连接请求数据的过程，增加了对门罗币协议消息头部的处理（33字节的解析）
    	modified:   node/node.go

--- node/node.go
+++ node/node.go
@@ -1,8 +1,9 @@
 package node
 
 import (
-	"bufio"
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"math/big"
@@ -10,6 +11,10 @@ import (
 	"os"
 	"strconv"
 )
+
+const LevinMessageHeaderLength = 33
+
+var LevinSignature = []byte{0x01, 0x21, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01}
 
 // 虚拟的门罗币节点
 type Node struct {
@@ -61,16 +66,75 @@ func (node Node) Start() {
 
 func handleConnectionRequest(conn net.Conn) {
 	defer conn.Close()
-	fmt.Println("Connected to " + conn.RemoteAddr().String())
+	fmt.Println("Accept incoming connection from " + conn.RemoteAddr().String())
 
-	reader := bufio.NewReader(conn)
 	for {
+		// 1. 创建一个大小为8字节的缓冲区，接收发送来的字节流数据的前8字节，前8字节为门罗币消息协议的levin signature
+		buffer := make([]byte, 33)
 		// 读取客户端发送的数据
-		message, err := reader.ReadString('\n')
+		header_length, err := conn.Read(buffer)
 		if err != nil {
-			log.Println("Error reading from connection: ", err)
+			log.Println("Error reading levin message from connection: "+conn.RemoteAddr().String(), err)
+			log.Println("Disconnect with connection " + conn.RemoteAddr().String())
 			return
 		}
-		fmt.Println("Receive message: " + message)
+		if header_length < LevinMessageHeaderLength {
+			log.Printf("Error levin message header length. Expected length: %d, received data length: %d\n", LevinMessageHeaderLength, header_length)
+			return
+		}
+		buffer_ptr := 0
+		if !bytes.Equal(buffer[buffer_ptr:buffer_ptr+8], LevinSignature) {
+			log.Println("Error receiving data from " + conn.RemoteAddr().String() + ": not Monero network protocol message.")
+			return
+		}
+		buffer_ptr += 8
+		fmt.Println("Great! Monero network protocol message!")
+
+		// 2. 继续接收后8个字节的数据，表示消息的数据长度
+		message_length := binary.LittleEndian.Uint64(buffer[buffer_ptr : buffer_ptr+8])
+		buffer_ptr += 8
+		fmt.Println("Monero network protocol message data length =", message_length)
+
+		// 3. 接收1个字节的bool类型的reture_data数据，0表示不需要回复(request数据)，1表示需要回复(response数据)
+		return_data := (buffer[buffer_ptr] != 0)
+		buffer_ptr += 1
+		if return_data {
+			fmt.Println("This message is a request, it needs to be response.")
+		} else {
+			fmt.Println("This message is a response, there is no need to reponse.")
+		}
+
+		// 4. 接收4字节的Command数据，uint32类型，其值代表着消息的类型，比如1001是握手消息，1002是定时同步消息，1003是ping/pong消息等
+		command := binary.LittleEndian.Uint32(buffer[buffer_ptr : buffer_ptr+4])
+		buffer_ptr += 4
+		fmt.Println("Message Command Type:", command)
+
+		// 5. 接收4字节的Return Code参数 int32类型
+		return_code := int32(binary.LittleEndian.Uint32(buffer[buffer_ptr : buffer_ptr+4]))
+		buffer_ptr += 4
+		fmt.Println("Return Code:", return_code)
+
+		// 6. 接收4字节的uint32类型Flags数据
+		flags := binary.LittleEndian.Uint32(buffer[buffer_ptr : buffer_ptr+4])
+		buffer_ptr += 4
+		fmt.Println("Flags:", flags)
+
+		// 7. Version参数，uint32类型，固定为1
+		version := binary.LittleEndian.Uint32(buffer[buffer_ptr : buffer_ptr+4])
+		buffer_ptr += 4
+		fmt.Println("Version:", version)
+
+		if buffer_ptr == LevinMessageHeaderLength {
+			fmt.Println("Header Parse complete!")
+		}
+
+		// n. 接收后续的数据
+		buffer = make([]byte, 1024)
+		remain_bytes_length, err := conn.Read(buffer)
+		if err != nil {
+			log.Println("Error reading remaining levin message from connection:"+conn.RemoteAddr().String(), err)
+		}
+		fmt.Printf("Remain data: %d bytes\n", remain_bytes_length)
+		fmt.Printf("%x\n", buffer[:remain_bytes_length])
 	}
 }
